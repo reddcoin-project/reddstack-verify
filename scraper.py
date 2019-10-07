f4c34c06df8bb69193d6b36e80d194f64c9e214b
Carson Miller <34222301+CarsonMiller2@users.noreply.github.com>
2019-10-07 16:25:40 -0700

    Create scraper.py
    This is the first version of the scraper. It is designed to work on any social media site. There is one known issue, which is that if the user does not have a reddID in their bio, but a reddID appears somewhere else on their page (like a retweet), there could be potential false positives.

---
+++ scraper.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+from reddstackverify.config import log
+
+IDENTIFIER = "reddid="
+
+'''
+# @params
+# url = a string representing a profile URL
+# reddid = a string representing the user's ReddID
+'''
+
+
+def scrape(url, reddid):
+
+    page = requests.get(url)
+
+    # Confirm page is valid
+    if page.status_code == 404:
+        log.info("Error: user profile not found.")
+        return 1
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+    found_id = None
+
+    for paragraph in soup.find_all('p'):
+        if IDENTIFIER in str(paragraph).lower():
+            found_id = str(paragraph.get_text())
+            # Remove excess bio text, if present
+            for item in found_id.split():
+                if IDENTIFIER in item.lower():
+                    found_id = item
+                    break
+
+    if found_id is None:
+        log.info("Error: no ID found.")
+        return 1
+
+    print(found_id)
+
+    if reddid not in found_id:
+        log.info("Error: ID found does not match input provided.")
+        return 1
+
+    return "url=" + url, found_id
